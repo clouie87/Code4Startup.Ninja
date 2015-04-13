039bec7562652d33a1cc0c4caa80dd53692aef39
clouie87 <carol.louie@gmail.com>
2015-04-13 09:50:44 -0300

    getting through task 4

---
+++ TaskNinja-Seed/scripts/services/comment.js
@@ -0,0 +1,14 @@
+'use strict';
+
+app.factory('Comment', function(FURL, $firebase){
+
+  var ref = new Firebase(FURL);
+
+
+  var Comment = {
+    comments: function(taskId) {
+      return $firebase(ref.child('comments').child(taskId)).$asArray();
+    },
+  };
+  return Comment;
+});
